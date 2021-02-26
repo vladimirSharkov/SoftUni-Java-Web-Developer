12f4edef3f90780f83b1ed06b4cdffaba02a04cc
vladimirSharkov <79506963+vladimirSharkov@users.noreply.github.com>
2021-02-26 17:53:49 +0200

    cookbook part1 update

--- JS Applications/Homeworks/02AsynchronousProgramming/Cookbook/app.js
+++ JS Applications/Homeworks/02AsynchronousProgramming/Cookbook/app.js
@@ -1,26 +1,28 @@
-function getRecipeList() {
+async function getRecipeList() {
     const url = 'http://localhost:3030/jsonstore/cookbook/recipes';
     const main = document.querySelector('main');
 
-    fetch(url)
-        .then(response => response.json())
-        .then(recipes => {
-            main.innerHTML = '';
-            Object.values(recipes).forEach(r => {
-                const result = e('article', {className: 'preview'},
-                    e('div', {className: 'title'}, e('h2', {}, r.name)),
-                    e('div', {className: 'small'}, e('img', {src: r.img}))
-                );
-                main.appendChild(result)
-            });
+    try {
+        const response = await fetch(url);
+        const recipes = await response.json();
+        main.innerHTML = '';
+        Object.values(recipes).forEach(r => {
+            const result = e('article', {className: 'preview'},
+                e('div', {className: 'title'}, e('h2', {}, r.name)),
+                e('div', {className: 'small'}, e('img', {src: r.img}))
+            );
+            main.appendChild(result)
         });
+    }catch (error) {
+        alert(error.message);
+    }
 }
 
 window.addEventListener('load', () => {
     getRecipeList()
 })
 
- function e(type, attributes, ...content) {
+function e(type, attributes, ...content) {
     const result = document.createElement(type);
 
     for (let [attr, value] of Object.entries(attributes || {})) {
