87c88f57dfb839634d6ddf4445d17b381a46445f
vladimirSharkov <79506963+vladimirSharkov@users.noreply.github.com>
2021-02-27 12:43:45 +0200

    exe02

---
+++ JS Applications/Homeworks/02.AsynchronousProgramming-Exercise/02.BusSchedule/app.js
@@ -0,0 +1,40 @@
+function solve() {
+    const departBtn = document.getElementById('depart');
+    const arriveBtn = document.getElementById('arrive');
+    const banner = document.querySelector('#info span');
+
+    let stop = {
+        next: 'depot'
+    }
+
+    async function depart() {
+        const url = 'http://localhost:3030/jsonstore/bus/schedule/' + stop.next;
+        const response = await fetch(url);
+        const date = await response.json();
+        console.log(date)
+
+        stop = date;
+
+        banner.textContent = `Next stop ${stop.name}`
+        departBtn.disabled = true;
+        arriveBtn.disabled = false;
+    }
+
+    async function arrive() {
+        const url = 'http://localhost:3030/jsonstore/bus/schedule/' + stop.next;
+        const response = await fetch(url);
+        const date = await response.json();
+
+        console.log(date)
+        banner.textContent = `Arriving at ${stop.name}`
+        departBtn.disabled = false;
+        arriveBtn.disabled = true;
+    }
+
+    return {
+        depart,
+        arrive
+    };
+}
+
+let result = solve();
