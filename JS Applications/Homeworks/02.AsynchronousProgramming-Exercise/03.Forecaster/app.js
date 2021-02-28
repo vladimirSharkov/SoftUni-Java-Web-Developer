b3db4b1ab273194497ef0a09ac623461f71cd7f9
vladimirSharkov <79506963+vladimirSharkov@users.noreply.github.com>
2021-02-28 09:40:21 +0200

    exe03 update

--- JS Applications/Homeworks/02.AsynchronousProgramming-Exercise/03.Forecaster/app.js
+++ JS Applications/Homeworks/02.AsynchronousProgramming-Exercise/03.Forecaster/app.js
@@ -4,26 +4,27 @@ function attachEvents() {
     const url = 'http://localhost:3030/jsonstore/forecaster/locations'
     const div = document.getElementById('forecast');
     const divParent = document.getElementById('current');
+    const upcomingDiv = document.getElementById('upcoming');
 
     btn.addEventListener('click', getWeather);
 
-     const conditionMap = {
-         'Sunny': '☀',
-         'Partly sunny':'⛅',
-         'Overcast':'☁',
-         'Rain':'☂',
-         'Degrees':'°'
-     }
+    const conditionMap = {
+        'Sunny': '☀',
+        'Partly sunny': '⛅',
+        'Overcast': '☁',
+        'Rain': '☂',
+        'Degrees': '°'
+    }
 
     async function getWeather() {
         const code = await getCode();
-        let [current,upcoming] =await Promise.all([
+        let [current, upcoming] = await Promise.all([
             getCurrent(),
             getForecast()
         ]);
 
         div.style.display = 'block';
-        const forecastsDiv= document.createElement('div');
+        const forecastsDiv = document.createElement('div');
         forecastsDiv.className = 'forecasts';
 
         const spanSymbol = document.createElement('span');
@@ -31,20 +32,19 @@ function attachEvents() {
         spanSymbol.textContent = conditionMap[current.forecast.condition];
 
 
-
         const spanCondition = document.createElement('span');
         spanCondition.className = 'condition';
 
-        const spanName  = document.createElement('span');
-        spanName.className = 'forecast-date';
+        const spanName = document.createElement('span');
+        spanName.className = 'forecast-data';
         spanName.textContent = current.name;
 
         const spanTemp = document.createElement('span');
-        spanTemp.className = 'forecast-date';
+        spanTemp.className = 'forecast-data';
         spanTemp.textContent = `${current.forecast.low}${conditionMap['Degrees']}/${current.forecast.high}${conditionMap['Degrees']}`;
 
         const spanDate = document.createElement('span');
-        spanDate.className = 'forecast-date';
+        spanDate.className = 'forecast-data';
         spanDate.textContent = current.forecast.condition;
 
 
@@ -56,6 +56,38 @@ function attachEvents() {
         forecastsDiv.appendChild(spanCondition)
 
         divParent.appendChild(forecastsDiv)
+
+        console.log(upcoming)
+        const forecastInfoDiv = document.createElement('div');
+        forecastInfoDiv.className = 'forecast-info';
+
+        Array.from(upcoming.forecast).forEach(upcoming => {
+
+            const upcomingSpan = document.createElement('span');
+            upcomingSpan.className = 'upcoming';
+
+            const symbolSpan = document.createElement('span');
+            symbolSpan.className = 'symbol';
+            symbolSpan.textContent = conditionMap[upcoming.condition];
+
+            const loHighSpan = document.createElement('span');
+            loHighSpan.className = 'forecast-date';
+            loHighSpan.textContent = `${upcoming.low}${conditionMap['Degrees']}/${upcoming.high}${conditionMap['Degrees']}`;
+
+
+            const conditionSpan = document.createElement('span');
+            conditionSpan.className = 'forecast-date';
+            conditionSpan.textContent = upcoming.condition;
+
+            upcomingSpan.appendChild(symbolSpan);
+            upcomingSpan.appendChild(loHighSpan);
+            upcomingSpan.appendChild(conditionSpan);
+
+            forecastInfoDiv.appendChild(upcomingSpan);
+
+        });
+
+        upcomingDiv.appendChild(forecastInfoDiv);
 
 
     }
@@ -85,7 +117,6 @@ function attachEvents() {
 
         return date;
     }
-
 
 
 }
